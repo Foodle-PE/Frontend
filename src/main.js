dd30233d04844316366a20da428b7673db100c4f
maria-mm0907 <U202317442@upc.edu.pe>
2025-05-12 21:54:55 -0500

    feat: add authentication protected

--- src/main.js
+++ src/main.js
@@ -1,5 +1,56 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from '../router/index.js'
+import './style.css'
 
-createApp(App).mount('#app')
+import { PrimeVue } from '@primevue/core'
+import Material from '@primeuix/themes/material'
+import 'primeflex/primeflex.css'
+import 'primeicons/primeicons.css'
+
+
+import ConfirmationService from 'primevue/confirmationservice'
+import DialogService from 'primevue/dialogservice'
+import ToastService from 'primevue/toastservice'
+
+
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Checkbox from 'primevue/checkbox'
+import Toast from 'primevue/toast'
+import Dialog from 'primevue/dialog'
+import InputGroup from "primevue/inputgroup";
+import InputGroupAddon from "primevue/inputgroupaddon";
+import Password from "primevue/password";
+import Divider from 'primevue/divider'
+
+import {createPinia} from "pinia";
+const pinia = createPinia();
+
+
+const app = createApp(App)
+
+
+
+app.use(PrimeVue, {
+    theme: { preset: Material },
+    ripple: true
+})
+
+app.use(ConfirmationService)
+app.use(DialogService)
+app.use(ToastService)
+    .use(pinia)
+
+app
+    .component('pv-button', Button)
+    .component('pv-input-text', InputText)
+    .component('pv-checkbox', Checkbox)
+    .component('pv-toast', Toast)
+    .component('pv-dialog', Dialog)
+    .component('pv-InputGroup', InputGroup)
+    .component('pv-InputGroupAddon', InputGroupAddon)
+    .component('pv-password', Password)
+    .component('pv-divider', Divider)
+app.use(router)
+app.mount('#app')
