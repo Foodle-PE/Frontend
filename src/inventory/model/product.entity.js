ac1fee6d731e53b5c93e9b4d95bc7eb4e4695e1c
Paolo <u20221f613@upc.edu.pe>
2025-05-13 19:30:03 -0500

    feat(inventory): add product.entity.js

---
+++ src/inventory/model/product.entity.js
@@ -0,0 +1,7 @@
+export default class Product {
+    constructor(name, expirationDate, quantity) {
+        this.name = name;
+        this.expirationDate = expirationDate;
+        this.quantity = quantity;
+    }
+}
