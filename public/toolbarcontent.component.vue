e74096ebeb55ad23d453a22537d252651fc3e152
maria-mm0907 <U202317442@upc.edu.pe>
2025-05-14 12:49:12 -0500

    feat: add toolbar content component

---
+++ public/toolbarcontent.component.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "toolbarcontent.component",
+})
+</script>
+
+<template>
+  <pv-toolbar class="toolbar">
+    <template #start>
+      <img src="../src/assets/logo_GoodFood.png" alt="GoodFood Logo" style="width: 100px; height: 92px;"/>
+    </template>
+
+  </pv-toolbar>
+
+</template>
+
+<style scoped>
+.toolbar {
+  display: flex;
+  justify-content: center;
+  background-color: #28A745;
+  height: 90px;
+  flex-wrap: nowrap;
+}
+
+</style>
