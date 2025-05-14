56643299d5b283bc9b08202a9c8be1b7c8027bcf
Luis Angel <1luis4mon@gmail.com>
2025-05-14 13:33:35 -0500

    services => alert.assembler.js

---
+++ src/alerts/services/alert.assembler.js
@@ -0,0 +1,10 @@
+import Alert from "../model/alert.entity.js";
+
+export async function fetchAlerts() {
+    const response = await fetch('http://localhost:3000/alerts')
+    if (!response.ok) {
+        throw new Error('No se pudieron obtener las alertas')
+    }
+    const rawAlerts = await response.json()
+    return rawAlerts.map(data => new Alert(data))
+}
