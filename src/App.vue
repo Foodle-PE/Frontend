d58cdbd266ae53b6aeb84900ea340f48d7df8a8d
Paolo <u20221f613@upc.edu.pe>
2025-05-14 23:40:21 -0500

    chore: COMMIT IMPORTANTE NO BORRAR

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,38 @@
 <script>
-import AlertList from './alerts/components/alerta-list.component.vue';
+import { defineComponent } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    AlertList
-  }
-}
-import {defineComponent} from "vue";
+import AlertList from './alerts/components/alerta-list.component.vue';
 import FooterContent from "../public/footer-content.vue";
 import SignInView from "../src/authorization/pages/login-content.component.vue";
 import HomeView from "./authorization/pages/home.component.vue";
-import toolbarcontentComponent from "../public/toolbarcontent.component.vue";
+import ToolbarContent from "../public/toolbarcontent.component.vue";
 import DashboardContent from "./views/dashboard-content.vue";
-
-
-import FooterContent from "/public/footer-content.vue";
 import InventoryTable from './inventory/components/inventory.table.component.vue';
+
 export default defineComponent({
-  components: {FooterContent, DashboardContent: DashboardContent},
-  components: {FooterContent, InventoryTable}
-  components: {toolbarcontentComponent, HomeView, FooterContent, SignInView},
-})
+  name: 'App',
+  components: {
+    AlertList,
+    FooterContent,
+    SignInView,
+    HomeView,
+    ToolbarContent,
+    DashboardContent,
+    InventoryTable
+  }
+});
 </script>
 
 <template>
   <div id="app">
     <h1>Gestión de Inventario</h1>
     <AlertList />
+    <ToolbarContent />
+    <DashboardContent />
+    <InventoryTable />
+    <FooterContent />
+    <router-view />
   </div>
-<DashboardContent></DashboardContent>
-  <InventoryTable></InventoryTable>
-  <FooterContent></FooterContent>
-<toolbarcontent-component/>
-  <router-view />
 </template>
 
 <style>
