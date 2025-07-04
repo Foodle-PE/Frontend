9d969d105024805af7f506730d6ee7041db90f79
Luiso-AM <140453696+Luiso-AM@users.noreply.github.com>
2025-07-04 16:51:30 -0500

    Update alert.assembler.js

--- src/alerts/services/alert.assembler.js
+++ src/alerts/services/alert.assembler.js
@@ -1,7 +1,8 @@
 import Alert from "../model/alert.entity.js";
 
 export async function fetchAlerts() {
-    const response = await fetch('http://localhost:3000/alerts')
+    //const response = await fetch('http://localhost:3000/alerts')
+    const response = await fetch('http://localhost:5104/api/Alerta')
     if (!response.ok) {
         throw new Error('No se pudieron obtener las alertas')
     }
