5280a578e14d055473e97ac31a402a8f50e6fd70
Paolo <u20221f613@upc.edu.pe>
2025-07-07 00:54:41 -0500

    chore: add env data

--- src/inventory/services/productService.js
+++ src/inventory/services/productService.js
@@ -1,6 +1,4 @@
 import http from '../../shared/services/http.js';
 
-const API_URL = 'http://localhost:5104/api/v1/inventory';
-
 export const getProducts = () => http.get('/inventory');
 export const addProduct = (product) => http.post('/inventory', product);
